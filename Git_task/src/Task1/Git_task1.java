a8a5726e89de83157ee1f1aed1cadbdfb954ac24
sukhppatekar <110394798+sukhppatekar@users.noreply.github.com>
2022-08-01 17:38:06 +0530

    updated code

--- Git_task/src/Task1/Git_task1.java
+++ Git_task/src/Task1/Git_task1.java
@@ -2,6 +2,10 @@ package Task1;
 
 public class Git_task1 
  {
+	public void m1()
+	{
+	System.out.println("m1---Git_task1");
+	}
  public static void main(String[] args) 
  {
 	System.out.println("Hello everyone");
